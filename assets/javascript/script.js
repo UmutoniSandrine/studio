5be281ae6559e9f26a737667990afbbf30a303eb
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-10-04 09:57:49 +0200

    59 commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
   
   $(document).ready(function(){
     $("form#form1").submit(function(event){
-      // event.preventDefault();
+      
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
